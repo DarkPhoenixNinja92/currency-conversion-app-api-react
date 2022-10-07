575007153a3d3223ec67039cecfa947d2839db6d
Shaun <scpour@gmail.com>
2022-10-07 16:18:52 -0400

    Display converted currency

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import './App.css';
 
 function App() {
   const api = 'https://v6.exchangerate-api.com/v6/2fefdf9a750573c0c4d6e10f/latest/USD';
+  const value = document.getElementById('currencyVal').value;
 
-  fetch(api)
+  fetch(api + new URLSearchParams({
+    key: api,
+    base_code: 'USD',
+    target_code: 'GBP'
+
+  }))
   .then(response => response.json())
   .then(res => console.log(res))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>How much money do you want to convert?</label>
+        <input type='number' id='currencyVal'/>
+        <input type='submit' value='Convert'/>
+      </form>
+      <h2>{` ${base_code} in ${target_code} is ${value}`}</h2>
     </div>
   );
 }
