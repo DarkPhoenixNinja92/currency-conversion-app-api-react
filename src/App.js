59f4f472b27eb5638f3cb6717159e5bd48145fe8
Shaun <scpour@gmail.com>
2022-10-08 10:46:39 -0400

    Added conversion rate attributes to api fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [conversionOut, setConversionOut] = useState(``)
+
   const api = 'https://v6.exchangerate-api.com/v6/2fefdf9a750573c0c4d6e10f/latest/USD';
   const value = document.getElementById('currencyVal').value;
 
   fetch(api + new URLSearchParams({
     key: api,
     base_code: 'USD',
-    target_code: 'GBP'
-
+    target_code: 'GBP',
+    conversion_rate: target_code.conversion_rate,
+    conversion_result: value / target_code.conversion_rate
   }))
   .then(response => response.json())
-  .then(res => console.log(res))
+  .then(res => console.log(res)
+  
+  )
   return (
     <div className="App">
       <form>
@@ -20,7 +25,7 @@ function App() {
         <input type='number' id='currencyVal'/>
         <input type='submit' value='Convert'/>
       </form>
-      <h2>{` ${base_code} in ${target_code} is ${value}`}</h2>
+      <h2>{setConversionOut(`${value} ${base_code} in ${target_code} is ${value}`)}</h2>
     </div>
   );
 }
